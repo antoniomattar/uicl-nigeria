8ab06894276daee2fc4fb0b54267269b1fae2aa8
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2022-09-01 21:09:11 +0300

    fixing the tailwindcss content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {},
   plugins: [
     require('@tailwindcss/aspect-ratio'),
