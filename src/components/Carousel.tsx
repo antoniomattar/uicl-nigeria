a58f7c7daec704fb0e75152714188468a4c1f096
antoniomattar <antoniomattar123@icloud.com>
2023-05-19 11:37:29 +0200

    test

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 export default function Carousel(props: any) {
   return (
-    <div className="carousel-center max-h-md carousel rounded-box max-w-md space-x-4 bg-neutral p-2">
+    <div className="max-h-md carousel-center carousel rounded-box max-w-md space-x-4 bg-neutral p-2">
       {props.images_list.map((image: any) => (
         <div className="carousel-item">
           <img src={image} className="rounded-box h-40" />
