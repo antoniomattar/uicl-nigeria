5035bd0fd1c251b44f34dfaf2282e9f85fee0332
antoniomattar <antoniomattar123@icloud.com>
2023-05-19 13:00:10 +0200

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer footer-center bg-base-300 p-4 text-base-content">
       <div>
-        <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © 2023 - All right reserved by UICL NIGERIA Ltd</p>
       </div>
     </footer>
   );
